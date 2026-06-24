661e603bcfa20690ee589c5a69d75c84a1e04d86
Sakshi-Agarwal34 <agarwalseema61912@gmail.com>
2026-06-24 14:17:27 +0530

    Add files via upload

---
+++ assignment0.py
@@ -0,0 +1,16 @@
+# 1) Practice print statement
+
+# Multiple objects
+print("Name:", "Ali", "Age:", 25)
+
+# Using separator
+print("Python", "Java", "C++", sep=" | ")
+
+# Using end
+print("Hello", end=" ")
+print("World!")
+
+# 2) Ask user to enter name and display greeting
+
+name = input("Enter your name: ")
+print("Hello,", name + "! Welcome!")
