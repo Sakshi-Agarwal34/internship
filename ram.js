5be1d27e618554a50b72e80165f150efbf2ca073
Sakshi-Agarwal34 <agarwalseema61912@gmail.com>
2026-08-10 09:33:45 +0530

    Add files via upload

---
+++ ram.js
@@ -0,0 +1,18 @@
+function checkFile(fileName) {
+    fetch(fileName)
+        .then(response => {
+            if (response.ok) {
+                console.log(fileName + " : File Exists");
+            } else {
+                console.log(fileName + " : File Not Found");
+            }
+        })
+        .catch(error => {
+            console.log(fileName + " : File Not Found");
+        });
+}
+
+// Test Cases
+checkFile("in.html");
+checkFile(style.css);
+checkFile(script.js);
